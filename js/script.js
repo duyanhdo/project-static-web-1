c2115a2902f77592329958a1ab7f6c63778f3754
quyetdv <50985862+quyetdv@users.noreply.github.com>
2019-09-18 15:46:26 +0700

    about.html

--- js/script.js
+++ js/script.js
@@ -2,5 +2,18 @@ $(function() {
 	// Load header.html, footer.html, menu.html
 	$('#header').load('header.html');
 	$('#footer').load('footer.html');
-	$('#menu').load('menu.html');
+	$('#sidebar').load('sidebar.html');
+	
+	// Back to Top
+	$(window).scroll(function() {
+    	if ($(this).scrollTop() >= 50) {        
+        	$('#return-to-top').fadeIn(200);    
+    	} else {
+        		$('#return-to-top').fadeOut(200); 
+    		}
+	});
+
+	$('#return-to-top').click(function() {      
+    	$('body,html').animate({scrollTop : 0}, 700);
+	});
 });
