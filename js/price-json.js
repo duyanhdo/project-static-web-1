4baeea6535dd3cd2d078b12220175baabfe0c873
quyetdv <50985862+quyetdv@users.noreply.github.com>
2019-10-21 21:45:30 +0700

    update bang gia

--- js/price-json.js
+++ js/price-json.js
@@ -1,11 +1,7 @@
 $(function() {
 	$.getJSON('price.json', function(data) {
 			console.log(data);
-			let free = data[0].free;
-			let bocrang = data[0].bocrang;
-			let niengrang = data[0].niengrang;
-			let implant = data[0].implant;
-
+			let free = data[0].free;			
 			free.forEach(function(element, index) {
 				$('#free').append(`
 				<tr>
@@ -15,7 +11,8 @@ $(function() {
 				</tr>
 				`)
 			});
-			
+
+			let bocrang = data[0].bocrang;
 			bocrang.forEach(function(element, index) {
 				$('#bocrang').append(`
 					<tr>
@@ -25,7 +22,7 @@ $(function() {
 					</tr>
 					`)
 			});
-			
+			let niengrang = data[0].niengrang;
 			niengrang.forEach(function(element, index) {
 				$('#niengrang').append(`
 					<tr>
@@ -35,7 +32,7 @@ $(function() {
 					</tr>
 					`)
 			});
-
+			let implant = data[0].implant;
 			implant.forEach(function(element, index) {
 				$('#implant').append(`
 					<tr>
@@ -45,8 +42,108 @@ $(function() {
 					</tr>
 					`)
 			});
-
-
+			let implant_all = data[0].implant_all;
+			implant_all.forEach(function(element, index) {
+				$('#implant_all').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let chinhhinh = data[0].chinhhinh;
+			chinhhinh.forEach(function(element, index) {
+				$('#chinhhinh').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let taytrang = data[0].taytrang;
+			taytrang.forEach(function(element, index) {
+				$('#taytrang').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let taohinh = data[0].taohinh;
+			taohinh.forEach(function(element, index) {
+				$('#taohinh').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let caorang = data[0].caorang;
+			caorang.forEach(function(element, index) {
+				$('#caorang').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let nhorang = data[0].nhorang;
+			nhorang.forEach(function(element, index) {
+				$('#nhorang').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let dieutrituy = data[0].dieutrituy;
+			dieutrituy.forEach(function(element, index) {
+				$('#dieutrituy').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let hanrang = data[0].hanrang;
+			hanrang.forEach(function(element, index) {
+				$('#hanrang').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let xuongham = data[0].xuongham;
+			xuongham.forEach(function(element, index) {
+				$('#xuongham').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			let kimcuong = data[0].kimcuong;
+			kimcuong.forEach(function(element, index) {
+				$('#kimcuong').append(`
+					<tr>
+					<td>${element.service}</td>
+					<td>${element.unit}</td>
+					<td>${element.cost}</td>
+					</tr>
+					`)
+			});
+			
+			
 	});
 	let results = [];
 			let compare = {
