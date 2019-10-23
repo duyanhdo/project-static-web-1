7df1d59a7d10a96b67c990cfb087a35d1ddeb8bc
quyetdv <50985862+quyetdv@users.noreply.github.com>
2019-10-23 19:16:51 +0700

    css

--- js/script.js
+++ js/script.js
@@ -29,29 +29,5 @@ $(function() {
 	}
     // Load WOW JS
     new WOW().init();
-    // Load See More
-    // let defaultHeight = 0;
-    // let text = $(".text");
-    // let textHeight = text[0].scrollHeight;
-    // let button = $(".button");
-    // button.text('Xem thêm');
-    // text.css({"max-height": defaultHeight, "overflow": "hidden"});
-
-    // button.on("click", function(){
-    //   let newHeight = 0;
-    //   if (text.hasClass("active")) {
-    //     newHeight = defaultHeight;
-    //     text.removeClass("active");
-    //     button.text('Xem thêm');
-    // } else {
-    //     newHeight = textHeight;
-    //     text.addClass("active");
-    //     button.text('Thu gọn');
-    // }
-    // text.animate({
-    //     "max-height": newHeight
-    // }, 1000);
-    // console.log(newHeight);
-    // });
    
 });
